2fe4bbaabfcfe89f7291309cad11a3776a1cb9ac
Marta Shevchenko <martochka.shevch@gmail.com>
2022-09-09 23:56:48 +0300

    lab1

---
+++ lab1/lab1.rb
@@ -0,0 +1,11 @@
+puts "Enter t"
+t = gets.chomp.to_f
+puts "Enter z"
+z = gets.chomp.to_f
+puts "Enter x"
+x = gets.chomp.to_f
+
+puts y = Math::exp(Math::tan(x)**2)+(Math::sqrt((t-z).abs))/(Math::cos(Math::PI**2)**3 + Math::exp(Math::PI)*z**2) + 2.3*10**1.6
+
+
+
